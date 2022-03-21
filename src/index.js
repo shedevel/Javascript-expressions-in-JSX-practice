b5f8c9d43473e230a7ef3ae52d38665abc5c051f
Sabina Oh <31659637+shedevel@users.noreply.github.com>
2022-03-21 11:28:05 +0100

    completed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+
 //It should display 2 paragraph HTML elements.
 //The paragraphs should say:
 //Created by YOURNAME.
@@ -6,3 +9,14 @@
 //E.g.
 //Created by Angela Yu.
 //Copyright 2019.
+
+const name = "SheDevel";
+const year = new Date().getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>Created by {name}.</p>
+    <p>Copyright {year}.</p>
+  </div>,
+  document.getElementById("root")
+);
